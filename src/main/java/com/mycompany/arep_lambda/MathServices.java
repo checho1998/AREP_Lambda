3bc0d714a60814b99cc30ceca5189d498655ee03
unknown <sergio.nunez@labinfo.is.escuelaing.edu.co>
2020-03-13 11:28:59 -0500

    primero

---
+++ src/main/java/com/mycompany/arep_lambda/MathServices.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.arep_lambda;
+
+/**
+ *
+ * @author sergio.nunez
+ */
+public class MathServices {
+    public static Integer square(Integer i){
+        return i*i;
+    }
+}
